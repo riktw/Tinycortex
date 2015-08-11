09bb42e1328bf12c7ff1e32a0ac94f6bf9b0ddb3
riktw <rik@justanotherelectronicsblog.com>
2015-08-11 22:23:33 +0200

    First real commit

---
+++ BlinkaLed/pinout.h
@@ -0,0 +1,43 @@
+
+
+#ifndef PINOUT_H
+#define PINOUT_H
+
+#include "mbed.h"
+
+#define IC1 P1_25
+#define IC2 P1_19
+#define IC3 P0_20
+#define IC4 P0_2
+#define IC5 P1_26
+#define IC6 P1_27
+#define IC7 P1_20
+#define IC8 P0_4
+#define IC9 P0_5
+#define IC10 P0_21
+#define IC11 P1_23
+#define IC12 P1_24
+#define IC13 P1_28
+#define IC14 P1_21
+#define IC15 P0_8
+#define IC16 P0_9
+#define IC17 P0_22
+#define IC18 P1_29
+#define IC19 P0_11
+#define IC20 P0_12
+#define IC21 P0_13
+#define IC22 P0_14
+#define IC23 P1_13
+#define IC24 P1_14
+#define IC25 P1_22
+#define IC26 P0_16
+#define IC27 P0_23
+#define IC28 P1_15
+#define IC29 P0_17
+#define IC30 P0_18
+#define IC31 P0_19
+#define IC32 P1_16
+#define IC33 P1_31
+#define LED1 P0_7
+
+#endif
